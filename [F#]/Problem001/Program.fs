04662e0429503b5354a1e95cd532e3877b6525d8
kirillito <creek13@gmail.com>
2015-10-02 11:36:28 -0300

    F# samples
    problems 1 and 2 in f#

---
+++ [F#]/Problem001/Program.fs
@@ -0,0 +1,11 @@
+﻿open System
+
+let check n = if ((n%3 = 0) || (n%5 = 0)) then n else 0
+
+let rec problem001 n = if (n > 0) then check (n) + problem001 (n-1) else 0
+
+let answer = problem001 (999)
+
+printfn "Problem 001: answer is %d" answer
+
+Console.ReadKey()
---
+++ [F#]/Problem002/Program.fs
@@ -0,0 +1,29 @@
+﻿open System
+open System.Numerics
+
+#light
+
+let sTicks = DateTime.UtcNow.Ticks
+
+let problem002 =
+    let fibs =
+        (1,1) |> Seq.unfold
+            (fun (n0,n1) ->
+                if n0 <= 4000000 then
+                    Some (n0, (n1, n0 + n1))
+                else
+                    None)
+    fibs |> Seq.fold
+        (fun acc x ->
+            if x % 2 = 0 then
+                acc + x
+            else
+                acc) 0
+
+let eTicks = DateTime.UtcNow.Ticks
+
+printfn "Problem 002: answer is %A" problem002
+printfn "Execution time: %d" (eTicks-sTicks)
+
+
+Console.ReadKey()
